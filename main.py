75d817452912905809e1275e0e76e4bef159382b
CoonandCo <coonandco.als@gmail.com>
2025-04-11 17:39:21 +0200

    On a battu laurens

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from restaurant import parse_restaurant
+from HENRIFUNC import extract_customer, extract_order
+from order_items import order_items
 import os
+import json
 
 if __name__ == "__main__":
     toto = []
-    for root, dirs, files in os.walk("deliveroo-20250411T130225Z-001"):
+    for root, dirs, files in os.walk("deliveroo"):
         for file in files:
             if file.endswith(".html"):
                 file_path = os.path.join(root, file)
-                restorant = parse_restaurant(file_path)
-                toto.append(restorant)
-            print(restorant)
+                restaurant = parse_restaurant(file_path)
+                customer = extract_customer(file_path)
+                order = extract_order(file_path)
+                oi = order_items(file_path)
+
+                order_dict = {
+                    "order": order,
+                    "customer": customer,
+                    "restaurant": restaurant,
+                    "order_items": oi
+                }
+
+                toto.append(order_dict)
+
+                print(toto)
+
+        with open("all_orders.json", "w", encoding="utf-8") as json_file:
+                json.dump(toto, json_file, indent=4, ensure_ascii=False)
